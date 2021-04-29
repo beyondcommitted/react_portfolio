b6b9d99cbca8fd2c5ba65cc4ed95b21bdc5fdc6c
Breon Sparrow <beyond.committed@gmail.com>
2021-04-29 01:43:10 -0700

    home page polished, about page started

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import './App.scss';
-import NavBar from './Components/NavBar';
+import "./App.scss";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+
 function App() {
-  
   return (
-    
+    <Router>
       <div className="App">
         <div className="sidebar">
           <NavBar />
         </div>
-          <div className="main-content">
-
+        <div className="main-content">
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/home" component={HomePage} />
+              <Route exact path="/about" component={AboutPage} />
+            </Switch>
           </div>
+        </div>
       </div>
-    )
-
+    </Router>
+  );
 }
 
 export default App;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { faInstagram, faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom'
 
-function HomePage() {
+export default function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am Breon
+                    <span> a Software Developer.</span>
+                </h1>
+                <p className="h-sub-text">
+                   Optimizing the whole. I love challenges and coding is certainly one of them. The feeling of gratification and overwhelming pride when you achieve a desired outcome is preeminent. I am determined to solve practical problems using software development. 
+                </p>
+                <div className="icons">
+                    <Link className='icon-holder'>
+                    <FontAwesomeIcon icon={faGithub} className='icon' />
+                    </Link>
+                    <Link className='icon-holder' src='https://www.linkedin.com/in/breon-sparrow-31bb24205/' alt='LinkedIn' target='_blank'>
+                    <FontAwesomeIcon icon={faLinkedin} className='icon' />
+                    </Link>
+                    <Link className='icon-holder'>
+                    <FontAwesomeIcon icon={faInstagram} className='icon' />
+                    </Link>
+                    <Link className='icon-holder'>
+                    <FontAwesomeIcon icon={faFacebook} className='icon' />
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }
 
-export default HomePage;
