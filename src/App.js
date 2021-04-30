987b17640c69e1f3d4355a9510f5cdf248131ab7
Breon Sparrow <beyond.committed@gmail.com>
2021-04-30 01:24:21 -0700

    About me presentable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
+import ProjectPage from "./Pages/ProjectPage";
+import BlogPage from "./Pages/BlogPage";
+import ContactPage from "./Pages/ContactPage";
 
 function App() {
   return (
@@ -15,8 +18,10 @@ function App() {
           <div className="content">
             <Switch>
               <Route exact path="/" component={HomePage} />
-              <Route exact path="/home" component={HomePage} />
               <Route exact path="/about" component={AboutPage} />
+              <Route exact path="/projects" component={ProjectPage} />
+              <Route exact path="/blogs" component={BlogPage} />
+              <Route exact path="/contact" component={ContactPage} />
             </Switch>
           </div>
         </div>
